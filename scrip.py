3c42b356db7e17002f564ffef3bdfe17821bb5fd
SlyLi <slylicn@qq.com>
2020-10-22 15:26:50 +0800

    1

--- scrip.py
+++ scrip.py
@@ -3,8 +3,9 @@ import os
 
 def read_dictory(path):
     for f_name in os.listdir(path):
-        print(f_name)
+        print('![](https://cdn.jsdelivr.net/gh/slyli/img1/wallpaper/%s)'%f_name)
 
 
 if __name__ == "__main__":
-    path = './wallpapper'
+    path = './wallpaper'
+    read_dictory(path)
